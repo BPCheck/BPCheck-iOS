34c24ed43f4261e16afdf566cce160d6417598ae
Ga-yo <39812@naver.com>
2021-04-05 08:59:30 +0900

    Create : custom MainFeedView

---
+++ BPCheck/BPCheck/Sourece/Components/MainFeedView.swift
@@ -0,0 +1,20 @@
+//
+//  MainFeedView.swift
+//  BPCheck
+//
+//  Created by 이가영 on 2021/04/05.
+//
+
+import UIKit
+
+class MainFeedView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
