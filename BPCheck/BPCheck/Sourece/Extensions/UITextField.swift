23115f3a68644807d4216b58d434e2efc4fcb06f
Ga-yo <39812@naver.com>
2021-04-04 11:59:13 +0900

    ADD : UITextField extension default setting

--- BPCheck/BPCheck/Sourece/Extensions/UITextField.swift
+++ BPCheck/BPCheck/Sourece/Extensions/UITextField.swift
@@ -5,4 +5,18 @@
 //  Created by 이가영 on 2021/04/04.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    
+    func defaultRoundTextField() {
+        keyboardType = UIKeyboardType.default
+        returnKeyType = UIReturnKeyType.done
+        autocorrectionType = UITextAutocorrectionType.no
+        font = UIFont.systemFont(ofSize: 13)
+        borderStyle = UITextField.BorderStyle.roundedRect
+        clearButtonMode = UITextField.ViewMode.whileEditing;
+        contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+    }
+    
+}
