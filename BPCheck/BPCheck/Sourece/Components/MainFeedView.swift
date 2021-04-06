1479f22cf03a9323f58ceb73297c9298729394bf
Ga-yo <39812@naver.com>
2021-04-06 22:59:15 +0900

    ADD : MainFeedView

--- BPCheck/BPCheck/Sourece/Components/MainFeedView.swift
+++ BPCheck/BPCheck/Sourece/Components/MainFeedView.swift
@@ -6,15 +6,61 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainFeedView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    let titleLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 27)
     }
-    */
+    
+    let contentLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 50)
+    }
+    
+    let iconLabel = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        clipsToBounds = true
+        layer.masksToBounds = true
+        layer.cornerRadius = 30
+        layer.borderColor = UIColor.lightGray.cgColor
+        layer.borderWidth = 0.5
+        
+        backgroundColor = .white
+        
+        addSubview(titleLabel)
+        addSubview(contentLabel)
+        addSubview(iconLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(snp.top).offset(20)
+            make.leading.equalTo(snp.leading).offset(30)
+        }
+        
+        contentLabel.snp.makeConstraints { (make) in
+            make.leading.lessThanOrEqualTo(snp.leading).offset(84)
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.trailing.lessThanOrEqualTo(snp.trailing).offset(-10)
+        }
+        
+        iconLabel.snp.makeConstraints { (make) in
+            make.trailing.equalTo(contentLabel.snp.leading).offset(-15)
+            make.centerY.equalTo(contentLabel)
+            make.height.equalTo(18)
+            make.width.equalTo(15)
+        }
+        
+    }
 
 }
