9f7ec281833e3a63b9603558a17de5cb09478fe3
Ga-yo <39812@naver.com>
2021-04-08 22:36:41 +0900

    ADD : TableChartsVC basic layout

--- BPCheck/BPCheck/Sourece/Modules/Chart/TableChartsViewController.swift
+++ BPCheck/BPCheck/Sourece/Modules/Chart/TableChartsViewController.swift
@@ -6,24 +6,57 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class TableChartsViewController: UIViewController {
 
+    private let tableView = UITableView()
+    
+    private let titleLabel = UILabel().then {
+        $0.text = "최저/고"
+        $0.font = UIFont.boldSystemFont(ofSize: 40)
+    }
+    
+    private let changeView = UIButton().then {
+        $0.setTitle("차트보기", for: .normal)
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        $0.tintColor = .white
+        $0.backgroundColor = MainColor.update
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 19
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        view.addSubview(titleLabel)
+        view.addSubview(changeView)
+            
+        setupConstraint()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupConstraint() {
+        titleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(view.snp.top).offset(140)
+            make.leading.equalTo(view.snp.leading).offset(30)
+        }
+        
+        tableView.snp.makeConstraints { (make) in
+            make.center.equalTo(view)
+            make.leading.equalTo(view)
+            make.trailing.equalTo(view)
+            make.width.equalTo(view.snp.width)
+            make.height.equalTo(300)
+        }
+        
+        changeView.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.top)
+            make.leading.equalTo(titleLabel.snp.trailing).offset(20)
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
     }
-    */
 
 }
