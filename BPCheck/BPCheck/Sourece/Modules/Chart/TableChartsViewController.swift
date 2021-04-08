dddf67e665e5112504d0417ccb69ef34c0def391
Ga-yo <39812@naver.com>
2021-04-08 22:34:33 +0900

    Create : TableChartsVC

---
+++ BPCheck/BPCheck/Sourece/Modules/Chart/TableChartsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableChartsViewController.swift
+//  BPCheck
+//
+//  Created by 이가영 on 2021/04/08.
+//
+
+import UIKit
+
+class TableChartsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
