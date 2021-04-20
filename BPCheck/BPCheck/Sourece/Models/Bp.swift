23b864383e5f9707ac2fda868440050953deb897
Ga-yo <39812@naver.com>
2021-04-20 10:51:28 +0900

    ADD : Bp model 추가

---
+++ BPCheck/BPCheck/Sourece/Models/Bp.swift
@@ -0,0 +1,37 @@
+//
+//  Bp.swift
+//  BPCheck
+//
+//  Created by 이가영 on 2021/04/20.
+//
+
+import Foundation
+
+struct Bp: Codable {
+    let lowBp: String
+    let highBp: String
+    let date: String
+    let pulse: String
+}
+
+struct AllBp: Codable {
+    let data: [Bp]
+}
+
+struct OnlyHighBp: Codable {
+    let data: [HighBp]
+}
+
+struct OnlyLowBp: Codable {
+    let data: [LowBp]
+}
+
+struct HighBp: Codable{
+    let highBp: String
+    let date: String
+}
+
+struct LowBp: Codable {
+    let lowBp: String
+    let date: String
+}
