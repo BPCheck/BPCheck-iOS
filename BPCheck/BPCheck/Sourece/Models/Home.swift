bbe7d7be94439a35a1420d885f21680d2dcabf3a
Ga-yo <39812@naver.com>
2021-04-20 10:51:38 +0900

    ADD : Home model 추가

--- BPCheck/BPCheck/Sourece/Models/Home.swift
+++ BPCheck/BPCheck/Sourece/Models/Home.swift
@@ -7,21 +7,14 @@
 
 import Foundation
 
-struct Hospital: Codable {
-    let hospitalName: String
-}
-
-struct Bp: Codable {
-    let lowBp: String
-    let highBp: String
-    let date: String
-    let pulse: String
-}
-
-struct Main: Codable {
+struct Home: Codable {
     let userId: String
     let password: String
     let name: String
     let hospitals: Hospital
     let bps: [Bp]
+}
+
+struct Main: Codable {
+    let main: Home
 }
