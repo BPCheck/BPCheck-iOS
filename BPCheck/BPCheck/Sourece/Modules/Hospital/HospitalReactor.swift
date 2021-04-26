fbf191c1699bf539eec11746d95180e5f39d193d
Ga-yo <39812@naver.com>
2021-04-26 10:09:39 +0900

    ADD : HospitalReactor getHospital 추가

--- BPCheck/BPCheck/Sourece/Modules/Hospital/HospitalReactor.swift
+++ BPCheck/BPCheck/Sourece/Modules/Hospital/HospitalReactor.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+import ReactorKit
+
+final class HospitalReactor: Reactor {
+    enum Action {
+        case load
+    }
+    
+    enum Mutation {
+        case setHosiptal(HospitalInfo)
+        case setError(String)
+    }
+    
+    struct State {
+        var hospital: HospitalInfo?
+        var result: String?
+    }
+    
+    let initialState: State
+    private let service = Service()
+    
+    init() {
+        initialState = State()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .load:
+            return service.getHospital().asObservable()
+                .map { hospital, response in
+                    switch response {
+                    case .ok:
+                        return .setHosiptal(hospital)
+                    case .conflict:
+                        return .setError("conflict")
+                    default:
+                        return .setError("server error")
+                    }
+                }
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setHosiptal(let data):
+            newState.hospital = data
+            newState.result = nil
+        case .setError(let error):
+            newState.hospital = nil
+            newState.result = error
+        }
+        return newState
+    }
+}
