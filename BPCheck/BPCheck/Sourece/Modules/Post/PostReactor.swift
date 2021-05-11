eac3d1ec9ce0e4b5a7c5d318669d2adc06cb00da
Ga-yo <39812@naver.com>
2021-05-11 20:58:09 +0900

    [ADD] PostReactor

--- BPCheck/BPCheck/Sourece/Modules/Post/PostReactor.swift
+++ BPCheck/BPCheck/Sourece/Modules/Post/PostReactor.swift
@@ -10,23 +10,51 @@ import ReactorKit
 import RxCocoa
 
 final class PostReactor: Reactor {
-    
     enum Action {
-        
+        case postBp(Bp)
     }
     
     enum Mutation {
-        
+        case setPopVC(Bool)
+        case setError(String)
     }
     
     struct State {
         var result: String?
+        var isDismiss: Bool
     }
     
     let initialState: State
     private let service = Service()
     
     init() {
-        initialState = State()
+        initialState = State(isDismiss: false)
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .postBp(let bp):
+            return service.postMeasureBp(bp.highBp, bp.lowBp, bp.pulse, bp.date).asObservable()
+                .map { response in
+                    switch response {
+                    case .ok:
+                        return .setPopVC(true)
+                    default:
+                        return .setError("이미 해당 날짜에 작성한 이력이 있습니다")
+                    }
+                }
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setPopVC(let access):
+            newState.isDismiss = access
+        case .setError(let error):
+            newState.result = error
+            newState.isDismiss = false
+        }
+        return newState
     }
 }
