0dc79f14c7705535f9478dfc4d3d557dd5aa7f47
Ga-yo <39812@naver.com>
2021-06-28 16:12:01 +0900

    [ADD] TabBarFlow

---
+++ BPCheck/BPCheck/Sourece/Flows/TabBarFlow.swift
@@ -0,0 +1,54 @@
+//
+//  TabBarFlow.swift
+//  BPCheck
+//
+//  Created by 이가영 on 2021/06/11.
+//
+
+import RxFlow
+import RxSwift
+import RxCocoa
+
+final class TabBarFlow: Flow {
+    private let services: Service
+    
+    private var rootViewController: UITabBarController
+    
+    var root: Presentable {
+        return rootViewController
+    }
+    
+    init(services: Service) {
+        self.services = services
+        self.rootViewController = UITabBarController()
+    }
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? BPCheckStep else { return .none }
+        
+        switch step {
+        case .tabBarIsRequired:
+            return navigateToTabBar()
+        default:
+            return .none
+        }
+    }
+}
+
+extension TabBarFlow {
+    private func navigateToTabBar() -> FlowContributors {
+        let homeFlow = HomeFlow(services: services)
+        
+        Flows.use(homeFlow, when: .created) { [unowned self] (root1) in
+            let tabBarItem1 = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+            root1.tabBarItem = tabBarItem1
+            
+            self.rootViewController.setViewControllers([root1], animated: false)
+        }
+        
+        return .multiple(flowContributors: [
+                    .contribute(withNextPresentable: homeFlow,
+                                withNextStepper: OneStepper(withSingleStep: BPCheckStep.homeIsRequired))
+                ])
+    }
+}
