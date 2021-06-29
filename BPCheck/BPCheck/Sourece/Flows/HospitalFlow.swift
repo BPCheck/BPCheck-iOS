13f8156a234f4b7d12711aa0d6cd197e287dd1cc
Ga-yo <39812@naver.com>
2021-06-29 16:20:24 +0900

    [ADD] HospitalFlow

---
+++ BPCheck/BPCheck/Sourece/Flows/HospitalFlow.swift
@@ -0,0 +1,50 @@
+//
+//  HospitalFlow.swift
+//  BPCheck
+//
+//  Created by 이가영 on 2021/06/16.
+//
+
+import Foundation
+import RxFlow
+import RxSwift
+import RxCocoa
+
+final class HospitalFlow: Flow {
+    private let services: Service
+    
+    private var rootViewController = UINavigationController().then {
+        $0.navigationController?.navigationBar.prefersLargeTitles = true
+        $0.title = "병원 목록"
+    }
+    
+    var root: Presentable {
+        return self.rootViewController
+    }
+
+    init(services: Service) {
+        self.services = services
+    }
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? BPCheckStep else { return .none }
+        
+        switch step {
+        case .hospitalIsRequired:
+            return .none
+        default:
+            return .none
+        }
+    }
+}
+
+extension HospitalFlow {
+    private func navigateToHospital() -> FlowContributors {
+        let reactor = HospitalReactor()
+        let viewController = HospitalViewController(reactor)
+        
+        self.rootViewController.setViewControllers([viewController], animated: true)
+
+        return .one(flowContributor: .contribute(withNextPresentable: viewController, withNextStepper: reactor))
+    }
+}
